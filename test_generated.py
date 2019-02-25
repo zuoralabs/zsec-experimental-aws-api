4e5af54e97b8ba0e9a451b35a47c0892f80bb86e
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2019-02-25 13:07:09 -0800

    WIP cont

--- test_generated.py
+++ test_generated.py
@@ -1,15 +1,16 @@
-import generated_module
+import generated.s3_types as aws_types
 import aws_meta
 
-rr = generated_module.CreateBucketRequest(Bucket='abctsstn0347010703456', ACL='private')
+rr = aws_types.CreateBucketRequest(Bucket='abctsstn0347010703456', ACL='private')
 
-def to_dict(nt):
-    return {k:v for k,v in nt._asdict().items() if v is not None}
+
+aws_types.CreateBucketRequest(Bucket="hi", )
+aws_types.CreateBucketRequest
+
 
 import boto3
 
 s3 = boto3.client('s3', region_name='us-east-1')
-#print(s3.create_bucket(**to_dict(rr)))
 
 #print(aws_meta.execute(s3, rr))
 print(rr.execute(s3))
